38f8dbbbb4b74a62cb8c8a6f9e5cce81a5f9f037
Cameron Smith <cameron85smith@gmail.com>
2022-05-16 13:39:04 +0200

    Added Operators

---
+++ Operators/Program.cs
@@ -0,0 +1,11 @@
+﻿var a = 1;
+var b = 2;
+var c = 3;
+
+Console.WriteLine((float)c / (float)b); // Floating point value.
+Console.WriteLine(a + b * c); // Operator Precedence.
+Console.WriteLine(a > b); // Comparison operator displays as boolean.
+Console.WriteLine(a == b); // Equality operator displays as boolean.
+Console.WriteLine(a != b); // Not Eqaul operator displays as boolean.
+Console.WriteLine(c > b && c > a); // Logical operator
+Console.WriteLine(c > b || c == a); // Or operator
