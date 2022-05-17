e3f1d00072a104fbb699d2c5fcd69c88fe9850e3
Cameron Smith <cameron85smith@gmail.com>
2022-05-17 10:18:40 +0200

    Added Strings solution

---
+++ Strings/Program.cs
@@ -0,0 +1,10 @@
+﻿var firstName = "Cameron";
+var lastName = "Smith";
+
+var fullName = string.Format("My full name is {0} {1} ", firstName, lastName);
+Console.WriteLine(fullName);
+
+var listOfNames = new string[3] {"Cameron", "Chanray", "Duncan"};
+
+var names = string.Join(", ", listOfNames);
+Console.WriteLine(names);
