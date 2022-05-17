bf02b50f29b961343ec96391c9d60118275e21de
Cameron Smith <cameron85smith@gmail.com>
2022-05-17 09:35:24 +0200

    Added Arrays solution

---
+++ Arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿var numbers = new int[3] {1, 2, 3};
+
+foreach (var value in numbers)
+    Console.WriteLine(value);
+
+var flags = new bool[2] {true, false};
+
+foreach (var flag in flags)
+    Console.WriteLine(flag);
+
+var names = new string[3] {"Cameron", "Chanray", "Duncan"};
+
+foreach (var name in names)
+    Console.WriteLine(name);
