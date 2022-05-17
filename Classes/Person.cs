8e88571c2555ec155e51fb4c80e55c1616028430
Cameron Smith <cameron85smith@gmail.com>
2022-05-17 08:44:58 +0200

    Added Classes solution

---
+++ Classes/Person.cs
@@ -0,0 +1,13 @@
+namespace Classes
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hello, my name is {0} {1}", FirstName, LastName);
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Classes.Math;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(); // If the class does not have the static keyword, you have to create an instance (object) of the class.
+            person.FirstName = "Cameron"; // if the class is static, we can initialise the properties directly afer adding the using statement for that specific class.
+            person.LastName = "Smith";
+            person.Introduce();
+
+            var calculator = new Calculator();
+            var result = calculator.Add(1, 3);
+            Console.WriteLine(result); // All methods of the Console class is static because we only have one console when running the application.
+        }
+    }
+}
